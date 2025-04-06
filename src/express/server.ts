9cd332eaa8cefe51cc560cc491def0172f713fab
Henrique Alves <henrique013.a@gmail.com>
2025-04-06 17:03:47 -0300

    Refactor server.ts

--- src/express/server.ts
+++ src/express/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.use('/', (await import('./routes/system.routes.js')).default)
+app.use((await import('./routes/system.routes.js')).default)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
